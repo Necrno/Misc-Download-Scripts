d5c9775d52f147c508a7f6a05306c015a607ee8f
simon <Anise5-humid5-Allow-5Lulu-Honk>
2017-12-17 10:44:14 -0500

    Improved viditut.com

--- viditut.com/crawler_courses.py
+++ viditut.com/crawler_courses.py
@@ -4,7 +4,15 @@ import json
 
 URL = "https://viditut.com"
 
-# https://viditut.com/ajax/course/573135/675578/play
+
+def request_timeout(url):
+    while True:
+        try:
+            return requests.get(url, timeout=20)
+        except:
+            print("!", end="", flush=True)
+            continue
+
 
 def get_categories():
 
@@ -30,7 +38,7 @@ def get_courses(category):
     while True:
 
         page += 1
-        r = requests.get("https://viditut.com/ajax/category/" + category[1] + "/courses?page=" + str(page))
+        r = request_timeout("https://viditut.com/ajax/category/" + category[1] + "/courses?page=" + str(page))
         soup = bs4.BeautifulSoup(json.loads(r.text)["html"], "html.parser")
 
         for link in soup.find_all("a"):
@@ -52,46 +60,14 @@ def get_courses(category):
     return courses
 
 
-def get_videos(course):
-
-    videos = []
-    r = requests.get(course[2])
-    soup = bs4.BeautifulSoup(r.text, "html.parser")
-
-    for link in soup.find_all("a"):
-
-        if link.get("class") is not None and str(link.get("class")) == "['item-name', 'video-name', 'ga']":
-            video_id = link.get("data-ga-value")
-            video_name = link.text.strip()
-
-            videos.append((video_name, video_id))
-
-    return videos
-
-
-def get_links(course, video):
-
-    links = []
-    r = requests.get("https://viditut.com/ajax/course/" + course[1] + "/" + video[1] + "/play")
-    json_obj = json.loads(r.text)[0]
-
-    for quality in json_obj["qualities"]:
-        links.append((quality, json_obj["urls"][quality]))
-
-    return links
-
-
-file = open("links.txt", "w")
+file = open("courses.txt", "w")
 
 for category in get_categories():
     print(category)
     for course in get_courses(category):
-
         print(course[0])
-
-        for video in get_videos(course):
-            for link in get_links(course, video):
-                file.write(category[0] + "/" + course[0] + "/" + video[0] + "\0" + link[0] + "\0" + link[1] + "\n")
+        file.write(category[1] + "\0" + course[0] + "\0" + course[1] + "\0" + course[2] + "\n")
+    file.flush()
 
 
 file.close()
